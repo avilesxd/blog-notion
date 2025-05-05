88f346c8e38272b62a44d2601669f801868f97b1
Ignacio Avilés <nacho72001@gmail.com>
2025-05-05 17:25:44 -0400

    Add Google Tag Manager and improve Google AdSense settings in the Container and _document components

--- components/Container.js
+++ components/Container.js
@@ -20,6 +20,16 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
     <div>
       <Head>
         <title>{meta.title}</title>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WVZR562M');`,
+          }}
+        />
         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-FF0MBVRHC5"></script>
         <script
@@ -35,7 +45,7 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
         {/* Google AdSense */}
         <meta name="google-adsense-account" content="ca-pub-5625393864502632"></meta>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5625393864502632"
-          crossorigin="anonymous"></script>
+          crossOrigin="anonymous"></script>
         {/* <meta content={BLOG.darkBackground} name="theme-color" /> */}
         <meta name="robots" content="follow, index" />
         <meta charSet="UTF-8" />
